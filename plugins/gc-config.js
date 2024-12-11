264500f51c5822b83f53df30627eede9a3243961
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2024-12-11 02:27:07 -0600

    ajustes

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -1,4 +1,13 @@
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix, command, botAdmin }) => {
+if (!m.isGroup) resp = `Esta accion solo puede ser usada en grupos`
+const bot = global.db.data.bot[conn.user.jid]
+const chats = bot.chats || {}
+const groups = chats.groups || {}
+const chat = m.isGroup ? groups[m.chat] || {} : privs[m.chat] || {}
+const users = m.isGroup ? chat.users || {} : privs || {}
+let user = m.isGroup ? users[m.sender] || {} : privs[m.sender] || {}
+var resp = ''
+if (botAdmin) {
 let isClose = { // Switch Case Like :v
 'open': 'not_announcement',
 'close': 'announcement',
@@ -7,16 +16,25 @@ let isClose = { // Switch Case Like :v
 'abrir': 'not_announcement',
 'cerrar': 'announcement',
 }[(args[0] || '')]
-if (isClose === undefined)
-throw `
+if (isClose === undefined) {
+resp = `
 *[❗] FORMATO ERRONEO!!*
 
 *┏━━━❲ ✨EJEMPLO✨ ❳━━━┓* 
 *┠┉↯ ${usedPrefix + command} abrir*
 *┠┉↯ ${usedPrefix + command} cerrar*
 `.trim()
+} else {
 await conn.groupSettingUpdate(m.chat, isClose)
-{m.reply('*[ ✔ ] GRUPO CONFIGURADO CORRECTAMENTE*')}
+resp = '*[ ✔ ] GRUPO CONFIGURADO CORRECTAMENTE*'
+if (isClose === ('cerrado' || 'cerrar' || 'close') && !botAdmin) chat.isBanned = true
+if (isClose === ('abierto' || 'abrir' || 'open') && !botAdmin) chat.isBanned = false
+
+}
+} else {
+resp = `No soy admin: no puedo realizar esta accion`
+}
+return conn.sendWritingText(m.chat, resp, m)
 }
 handler.help = ['group open / close', 'grupo abrir / cerrar']
 handler.tags = ['group']
--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -14,18 +14,70 @@ WA_DEFAULT_EPHEMERAL
 } = (await import('@whiskeysockets/baileys')).default;
 import * as fs from 'fs'
 let handler = async (m, { conn, text, participants, isOwner, isAdmin, command }) => {
-let texto = text.split(command)[0]//text.replace(command, '').trim()
-let quoted = m.quoted ? m.quoted : m
+if (m.isBaileys) return
+try {
 let users = participants.map(u => conn.decodeJid(u.id))
 let q = m.quoted ? m.quoted : m || m.text || m.sender
 let d = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
 let generarElWAMensajeDesdeElContenido = await generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? d.message[q.mtype] : { text: '' || d }}, { quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100, userJid: conn.user.id })
 let msg = conn.cMod(m.chat, generarElWAMensajeDesdeElContenido, text || q.text, conn.user.jid, { mentions: users })
-if (texto !== undefined) {
-
-} else if (quoted.text !== undefined) {
-
+try {
+if (msg.message.extendedTextMessage && msg.message.extendedTextMessage.text !== undefined) {
+let txt = '';
+let count = 0;
+for (const c of msg.message.extendedTextMessage.text) {
+await new Promise(resolve => setTimeout(resolve, 15));
+txt += c;
+count++;
+if (count % 10 === 0) {
+ await conn.sendPresenceUpdate('composing' , m.chat);
 }
+}
+await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else if (msg.quoted && msg.mtype) {
+return conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }, {quoted: m, ephemeralExpiration: 2*60*1000})
+}
+console.log('texto: ', msg.message)
+} catch (error) {
+console.log('texto: ', msg.message + error)
+return conn.sendWritingText(m.chat, error, m)
+//await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }, {quoted: m, ephemeralExpiration: 2*60*1000, disappearingMessagesInChat: 24*60*100})
+}
+} catch (e) {
+console.log('texto error: ', e)
+
+
+//[ By @NeKosmic || https://github.com/NeKosmic/ ]
+
+
+let users = participants.map(u => conn.decodeJid(u.id))
+let quoted = m.quoted ? m.quoted : m
+let mime = (quoted.msg || quoted).mimetype || ''
+let isMedia = /image|video|sticker|audio/.test(mime)
+let more = String.fromCharCode(8206)
+let masss = more.repeat(850)
+let htextos = `${text ? text : "*Hola :D*: " + e }`
+const messageFinal = {extendedTextMessage: {
+text: `${masss}\n${htextos}\n`,
+ ...{ contextInfo: { 
+mentionedJid: users, externalAdReply: { thumbnail: imagen1, sourceUrl: md }}}}}
+if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m, ephemeralExpiration: 2*60*1000 })
+} else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m, ephemeralExpiration: 2*60*1000 })
+} else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m, ephemeralExpiration: 2*60*1000 })
+} else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) {
+var mediax = await quoted.download?.()
+conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: m, ephemeralExpiration: 2*60*1000 })
+} else {
+await conn.relayMessage(m.chat, messageFinal, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+}
+}
+
 }
 handler.command = /^(hidetag|notificar|notify)$/i
 handler.group = true
