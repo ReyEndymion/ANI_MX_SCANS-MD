01cbe8aad1c402306dac6a90f05f1d2962925492
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2025-08-16 13:36:23 -0600

    Update gc-fantasmas.js

--- plugins/gc-fantasmas.js
+++ plugins/gc-fantasmas.js
@@ -1,57 +1,53 @@
-const {  Low, JSONFile  } = await import('lowdb');
-const databaseFile = './countMessagesReg.json';
-const adapter = new JSONFile(databaseFile);
-const db = new Low(adapter);
-let handler = async (m, {conn, text, participants, db, userdb, senderJid}) => {
-await db.read();
-let chat = db.data.bot[conn.user.jid].chats.groups[m.chat]
-let resp = '', ghost = false
-let participantIds = new Set(participants.map(u => u.id));
-let users = chat.users || {}
+import { plugins, getCommandVariants, wrapText, delay } from '../lib/functions.js'
+let usersToGhost = [];
+let tags = '', isWelcome = false
+let handler = async (m, {conn, db, text, usedPrefix, command, groupMetadata, participants, chatdb, usersdb, userdb, senderJid}) => {
+if (!chatdb.isCountMsgs) return conn.sendWritingText(m.chat, `El contador en este chat está desactivado Es posible Que no existan datos Para los usuarios así que actívelo usando ${usedPrefix}enable countmsg Y Espere a que los usuarios Hagan mensajes`, userdb, m)
+const {lid, userID} = await import('../config.js')
+let resp = ''
+const groupLid = participants.some((u) => u.id.endsWith(lid))
+let participantIds = new Set(groupLid ? participants.map(u => u.jid) : participants.map(u => u.id));
 var sum = participants.length
 
 for (let participant of participants) {
-let user = participant.id;
-console.log('mentioned: ', users[user] && users[user].msgcount && users[user].msgcount.count)
-if (!(user in users)) {
-db.data.bot[conn.user.jid].chats.groups[m.chat].users[user] = {
+if (!participant.id.endsWith(userID)) delete usersdb[participant.id]
+let userId = groupLid ? participant.jid : participant.id;
+if (!(userId in usersdb)) {
+usersdb[userId] = {
 msgcount: {
 count: 0,
 time: 0
 }
 };
 await db.write();
-console.log(`Usuario ${user} inicializado en la base de datos.`);
-} else if ((users[user] && users[user].msgcount && users[user].msgcount.count && users[user].msgcount.time) === undefined) {
-db.data.bot[conn.user.jid].chats.groups[m.chat].users[user] = {
-msgcount: {
-count: 0,
-time: 0
+console.log(`Usuario ${userId} inicializado en la base de datos.`);
+} else {
+if (usersdb[userId.endsWith(lid)]) {
+delete usersdb[userId.endsWith(lid)] 
+await db.write();
 }
 }
 }
+for (let userId in usersdb) {
+if (!participantIds.has(userId) || typeof usersdb[userId] !== 'object' || usersdb[userId] === null) {
+delete usersdb[userId];
+await db.write();
 }
-//
-for (let user in users) {
-if (!participantIds.has(user) || typeof users[user] !== 'object' || users[user] === null) {
-delete users[user];
 }
-}
-let usersToGhost = [];
-let tags = ''
-for (let usuario in users) {
-let user = users[usuario];
+if (/^(verfantasmas|fantasmas|sider|Sider)$/i.test(command)) {
+for (let usuario in usersdb) {
+let user = usersdb[usuario];
 let tag = usuario.split('@')[0];
 
 if (user && user.msgcount && user.msgcount.count === 0) {
-usersToGhost.push(usuario); // Agregar usuario a la lista de eliminación
+usersToGhost.push(usuario);
 let count = user.msgcount.count
 tags += `@${tag} con ${count} mensajes\n`;
 }
 }
 
 if (usersToGhost.length > 0) {
-const date = new Date(users[senderJid].msgcount.time);
+const date = new Date(usersdb[senderJid].msgcount.time);
 const day = date.getDate();
 const monthNames = [
 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
@@ -61,35 +57,75 @@ const month = monthNames[date.getMonth()];
 const year = date.getFullYear();
 const formattedDate = `${day} de ${month} de ${year}`;
 resp = `*[REVISIÓN DE INACTIVOS]*\n\n*Grupo: ${await conn.getName(m.chat)}*\n*Participantes: ${sum}*\n\n*[👻 LISTA DE FANTASMAS 👻 ]*\n${tags}\n\n*Nota: Esto es 100% acertado y el conteo empezo desde ${formattedDate}*`
-ghost = true
-} else {
-resp = `*Este grupo no tiene fantasmas :D.*`;
-ghost = false
-}
-let txt = '';
-let count = 0;
-for (const c of resp) {
-await new Promise(resolve => setTimeout(resolve, 5));
-txt += c;
-count++;
-if (count % 10 === 0) {
-conn.sendPresenceUpdate('composing' , m.chat);
-}
-}
-if (ghost) {
 await conn.sendWritingText(m.chat, resp, userdb, m)
 } else {
+resp = `*Este grupo no tiene fantasmas :D.*`;
 return conn.sendWritingText(m.chat, resp, userdb, m)
 }
 }
-handler.command = /^((ver)?fantasmas|sider|ghosts)$/i
+if (/^(kickfantasmas|sacarfantasmas)$/i.test(command)) {
+if (usersToGhost.length > 0) {
+resp = `*[ELIMINACION DE INACTIVOS]*\n\n*Grupo: ${await conn.getName(m.chat)}*\n*Participantes: ${sum}*\n\n*[ 👻 FANTASMAS QUE MORIRAN 👻 ]*\n${tags}\n\n*Nota: Esto es 100% acertado y el Bot eliminara la lista mencionada, empezando en 20 segundos y cada 10 segundos eliminara un numero*`
+let {key} = await conn.sendWritingText(m.chat, resp, userdb, m)
+await delay(1 * 20000)
+
+if (chatdb.welcome) {
+chatdb.welcome = false
+isWelcome = true
+}
+try {
+const emottime = ['🕛','🕧','🕐','🕜','🕑','🕝','🕒','🕞','🕓','🕟','🕔','🕠','🕕','🕡','🕖','🕢','🕗','🕣','🕘','🕤','🕙','🕥','🕚','🕦']
+let userCount = 1
+for (let user of usersToGhost) {
+let isAdmin = participants.some(v => (groupLid ? v.jid === user : v.id === user) && v.admin);
+if (isAdmin) {
+await conn.sendEditWritingText(m.chat, `⚠️ El admin @${user.split('@')[0]} no lo puedo eliminar, sacalo por tu cuenta.`, key, userdb, m)
+await delay(1 * 10000)
+continue
+}
+await conn.sendReact(m.chat, `${userCount}️⃣`, m.key)
+await conn.sendEditWritingText(m.chat, `Eliminando a @${user.split('@')[0]}`, key, userdb, m)
+await delay(1 * 10000)
+await conn.sendReact(m.chat, `⚰️`, key)
+await conn.sendReact(m.chat, `🪦`, key)
+
+let res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+console.log('fantasmas: ', res[0].status)
+await delay(1 * 10000)
+if (res[0].status === "200") {
+await conn.sendReact(m.chat, `✅`, m.key)
+await conn.sendEditWritingText(m.chat, `Listo ✅`, key, userdb, m)
+await conn.sendReact(m.chat, `✅`, key)
+} else if (res[0].status === "406") {
+await conn.sendEditWritingText(m.chat, `El creador es inmutable, no se puede eliminar`, key, userdb, m) 
+} else if (res[0].status === "404") {
+await conn.sendEditWritingText(m.chat, `A que puto, se salio antes 👺👎`, key, userdb, null)
+}
+}
+} finally {
+if (isWelcome) {
+chatdb.welcome = true
+isWelcome = false
+}
+usersToGhost = []
+}
+
+} else {
+return conn.sendWritingText(m.chat, ` Debe ejecutar el comando *${usedPrefix}fantasmas* antes de continuar con la eliminación`, userdb, m)
+}
+}
+}
+handler.command = /^(ver|kick|sacar)?(fantasmas|sider|Sider)$/i
 handler.group = true
-//handler.botAdmin = true
-//handler.admin = true
+handler.botAdmin = true
+handler.admin = true
 handler.help = [];
 handler.tags = [];
-handler.menu = [];
-handler.type = "";
+handler.menu = [
+{title:"💎 FANTASMAS", description: `Busca y encuentra gente inactiva en el grupo usando:\n\n${getCommandVariants(handler.command).map(hc => `#${hc}`).join('\n')}`, id: `fantasmas`},
+{title:"💎 ELIMINAR FANTASMAS", description: "elimina a los usuarios inactivos del grupo usando #kickfantasmas", id: `kickfantasmas`}
+];
+handler.type = "gadmin";
 handler.disabled = false;
 
 export default handler
