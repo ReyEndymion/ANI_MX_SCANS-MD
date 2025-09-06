f412b39320816881ac1ba1975b13103f62db78cf
Rey Endymion <33460024+ReyEndymion@users.noreply.github.com>
2025-09-06 14:38:47 -0600

    Update herramientas-encuesta.js

--- plugins/herramientas-encuesta.js
+++ plugins/herramientas-encuesta.js
@@ -1,20 +1,20 @@
-let handler = async (m, {conn, text, args, usedPrefix, command, participants, db, userdb, senderJid}) => {
+let handler = async (m, {conn, text, args, usedPrefix, command, groupMetadata, participants, db, userdb, senderJid, objs}) => {
+const {dbGroups, inMstore} = objs
+const {userID, groupID} = await import('../config.js')
 let name = await conn.getName(senderJid);
 let who = m.mentionedJid//
 let menciones = [];
-let txto = text; // Texto formateado en proceso
-let mentions = [...text.matchAll(/@\d+/g)]; // Buscar menciones
+let txto = text;
+let mentions = [...text.matchAll(/@(\d{5,20}(?:-\d+)?)(@g\.us)?/g)];
+const isAnnounce = groupMetadata?.announce 
 
 for (const mention of mentions) {
+if (mention[0].endsWith(groupID)) await dbGroups.read()
 const phoneNumber = mention[0].replace('@', '');
-const name = await conn.getName(`${phoneNumber}@s.whatsapp.net`);
+const jid = phoneNumber.endsWith(groupID) ? phoneNumber : phoneNumber + userID 
+let name = mention[0].endsWith(groupID) ? (dbGroups.data[jid]?.subject == undefined ? inMstore.chats[jid]?.subject === undefined ? jid : inMstore.chats[jid].subject :  dbGroups.data[jid].subject || jid ) : await conn.getName(jid);
+if (name === undefined) name = 'Grupo';
 txto = txto.replace(new RegExp(mention[0], 'g'), name);
-}
-for (const jid of who) {
-let mencion = `${conn.getName(jid)} `;
-menciones += mencion
-
-//let names = await conn.getName(phoneNumber);
 }
 let num = []
 for (const jid of who){
@@ -23,40 +23,24 @@ num += `@${phoneNumber} `
 }
 if (name === 'undefined') name = 'Sin nombre';
 
-let int = text//.includes(menciones)//(who.filter(entry => typeof entry[0] === 'string' && !isNaN(entry[0])).map(entry => ({ jid: entry[0] })).slice(0).map(({jid}) => `${conn.getName(jid)}`).join` y `)//.replace(who, menciones)
 
-const parts = txto.split('|')
-/*for (let i = 0; i < text.length; i++) {
-text = parts.replace(new RegExp(`@${who[i].replace('@s.whatsapp.net', '')}\\b`, 'g'), menciones[i]);
-} */
+const parts = txto.replace(/^\|+|\|+$/g, '').split('|')
 if (parts.length < 4) {
 let resp = `*[❗INFO❗]*\n*FORMATO DE USO (múltiples selecciones)*\n\n${usedPrefix + command} 0|Pregunta?|Opcion1|Opcion2...\n\n*FORMATO DE USO (una sola selección)*\n\n${usedPrefix + command} 1|Pregunta?|Opcion1|Opcion2...`
-
 return conn.sendWritingText(m.chat, resp, userdb, m)
 }
 
-//let selectableCount = 0; 
 const selectableCountPart = parts[0];
-let question = parts[1]//.replace(new RegExp(`${num}\\b`, 'g'), menciones)//.replace(num, menciones)//.includes(menciones).join(' ');
-/*for (let i = 0; i < who.length; i++) {
-const phoneNumber = who[i].replace('@s.whatsapp.net', ''); // Obtener el número de teléfono sin @s.whatsapp.net
-question = question.replace(new RegExp(`${num}\\b`, 'g'), menciones);
-}*/
-
+let question = parts[1]
 let options = parts.slice(2);
 const selectableCount = parseInt(selectableCountPart); 
-console.log('quien', txto)
 
 if (!/^\d+$/.test(selectableCountPart)) {
 let resp = "*[❗INFO❗] El primer valor debe ser un número sin espacios.*";
-
 return conn.sendWritingText(m.chat, resp, userdb, m)
 }
-//if (args.length > 0) {
-//selectableCount = parseInt(args[0]);
 if (isNaN(selectableCount) || (selectableCount !== 0 && selectableCount !== 1)) {
 let resp = "*[❗INFO❗] El primer valor debe ser 0 (múltiples selecciones) o 1 (una sola selección).*";
-
 return conn.sendWritingText(m.chat, resp, userdb, m)
 }
 //}
@@ -77,8 +61,10 @@ return conn.sendMessage(m.chat, poll, { quoted: m, ephemeralExpiration: 24*60*10
 handler.help = ['encuesta question|option|option']
 handler.tags = ['group']
 handler.command = ['poll', 'encuesta']
-handler.menu = [];
-handler.type = "";
+handler.menu = [
+{title:"💎 ENCUESTA", description: "realiza una encuesta en el grupo usando #encuesta <0|1> | pregunta? | opción1 | opción2 ...", id: `encuesta`}
+];
+handler.type = "herramientas";
 handler.disabled = false;
 
 export default handler;
